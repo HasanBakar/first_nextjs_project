53cef8fdf8a2720107c584cc4866e8c1d7cc17df
HasanBakar <abu.bakar.cse.uctc@gmail.com>
2023-09-30 00:06:44 +0600

    home completed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,25 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.css';
+import hero from "../../public/hero.png";
+
+
 
 export default function Home() {
   return (
-   <div>
-    Home
-   </div>
-  )
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. we bring together the teams from the
+          global Tech industry.
+        </p>
+        <button className={styles.button}>See our works</button>
+      </div>
+      <div className={styles.item}>
+        <Image className={styles.img} src={hero} width={500} height={500} alt="hero section image" />
+      </div>
+    </div>
+  );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,10 +7,10 @@ function Footer() {
       <div className={styles.container}>
         <div>©2023 Hasan. All rights reserved.</div>
         <div className={styles.socials}>
-          <Image src="/1.png" className={styles.icons} width={15} height={15} alt="facebook" />
-          <Image src="/2.png" className={styles.icons} width={15} height={15} alt="Instagram" />
-          <Image src="/3.png" className={styles.icons} width={15} height={15} alt="Twitter" />
-          <Image src="/4.png" className={styles.icons} width={15} height={15} alt="Youtube" />
+          <Image src="/1.png" className={styles.icons} width={15} height={15} alt="facebook account" />
+          <Image src="/2.png" className={styles.icons} width={15} height={15} alt="Instagram account" />
+          <Image src="/3.png" className={styles.icons} width={15} height={15} alt="Twitter account" />
+          <Image src="/4.png" className={styles.icons} width={15} height={15} alt="Youtube handle" />
         </div>
       </div>
     );
